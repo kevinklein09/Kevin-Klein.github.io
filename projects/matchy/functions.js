679e291eb71c49c4444eb7000f12d3e07ffdfec3
Kevin Klein <kklein166@gmail.com>
2022-10-08 15:20:18 +0000

    adding pseudocode to matchy

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,13 +13,13 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function search(array, string) {
-    for(var i = 0; i < array.length; i++) {
-        if(array[i].name === string) {
-            return array[i];
+    for(var i = 0; i < array.length; i++) { // create a for loop to iterate through array
+        if(array[i].name === string) { // create an if statement to determine if the input string is stictly equal to the array name property in each iteration
+            return array[i]; // return the object in the array if the if statement is true
         }
 
         }
-        return null;
+        return null; // otherwise, return null
         
     }
     
@@ -30,9 +30,9 @@ function search(array, string) {
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function replace(array, string, object) {
-    for(var i = 0; i < array.length; i++) {
-        if(array[i].name === string) {
-            return array[i] = object;
+    for(var i = 0; i < array.length; i++) { // create a for loop to iterate through the array
+        if(array[i].name === string) { //create an if statement to determine if the input string is stictly equal to the array name property in each iteration 
+            return array[i] = object; //if true, replace the object at the particular iteration with the input object
         }
 
         }
@@ -44,9 +44,9 @@ function replace(array, string, object) {
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function remove(array, string) {
-    for(var i = 0; i < array.length; i++) {
-        if(array[i].name === string) {
-            return array.splice(i, 1);
+    for(var i = 0; i < array.length; i++) { // create a for loop to iterate through the array
+        if(array[i].name === string) { //create an if statement to determine if the input string is stictly equal to the array name property in each iteration
+            return array.splice(i, 1); // remove the 
         }
     }
 }
@@ -56,11 +56,11 @@ function remove(array, string) {
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function add(array, object) {
-    if(object.name.length > 0 && object.species.length > 0) {
-        for(var i = 0; i < array.length; i++) {
-            if(array[i].name !== object.name) {
-                return array.push(object);
-            } else {
+    if(object.name.length > 0 && object.species.length > 0) { //create an if statement to determine if the animal object has a name AND species property witha length greater than 0
+        for(var i = 0; i < array.length; i++) { //create a for loop to iterate through the array
+            if(array[i].name !== object.name) { //create an if statement to determine if the array contains the input object name already by using the strictly not equal operator
+                return array.push(object); //if true, push the object into the array
+            } else { //if false, return null
                 return null;
             }
         }
