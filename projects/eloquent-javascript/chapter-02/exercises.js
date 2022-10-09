8f2fd2e2d56696e513f2076da2a4c0b633444825
Kevin Klein <kklein166@gmail.com>
2022-10-09 02:04:25 +0000

    studies - variables

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -3,25 +3,26 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function triangles(number) {
+function triangles(number) { //create a function triangles that has one input, number
+  //create a for loop that starts at a single hashtag, stops at the given input number, and increments one hashtag at each iteration
   for(var triangleLoop = "#"; triangleLoop.length <= number; triangleLoop += "#") {
-    console.log(triangleLoop);
+    console.log(triangleLoop); //log triangleLoop to console to get a triangle of hashtags 
   }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz() {
-  for(var i = 1; i <= 15; i++) {
-    if(i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzbuzz");
-    } else if(i % 3 === 0) {
-      console.log("fizz");
-    } else if(i % 5 === 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
+function fizzBuzz() { //create a function fizzBuzz
+  for(var i = 1; i <= 15; i++) { //create a for loop to increment numbers 1-15
+    if(i % 3 === 0 && i % 5 === 0) { //create an if statement to determine if the index i is divisible by 3 AND 5 both
+      console.log("fizzbuzz"); //if true, log 'fizzbuzz' to the console
+    } else if(i % 3 === 0) { //create else if statement to determine if the index i is divisible by 3
+      console.log("fizz"); //if true, log 'fizz' to the console
+    } else if(i % 5 === 0) { //create else if statement to determine if the index i is divisible by 5
+      console.log("buzz"); //if true, log 'buzz' to the console
+    } else { //create else statement
+      console.log(i); //log i to console as default
     }
   }
 }
@@ -30,8 +31,19 @@ function fizzBuzz() {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard(number) { //create a function drawwChessBoard that takes one input, number
+  var board = ""; //create a variable board and assign its value to an empty string to store the result
+  for(var y = 0; y < number; y++) { //create a for loop that iterates through input number to create the rows of the board
+    for(var x = 0; x < number; x++) { //create a for loop that iterates through number to create the columns of the board
+      if ((y + x) % 2 === 0) { //create an if statement that determines if the sum of y + x is divisible by 2. Every other number will be divisible by 2
+        board += " "; //if true, a single space is added to board
+      } else { //create an else statement
+        board += "#"; 
+      }
+    }
+    board += "\n";
+  }
+  return board;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- studies/variables.js
+++ studies/variables.js
@@ -1,8 +1,29 @@
 /**
  * VARIABLES:
  * 
- * 0: Variables are named containers for data in Javascript...
- * 
- * 1:
- * 
+ * 0: Variables are named containers that are used to store data in Javascript. *
+ They can be thought of as placeholders that can be accessed later in our program. *
+ The data that is stored inside a variable is referred to as the value. There *
+ are many data types that can be stored as the value of a variable, such as a *
+ String, Number, Boolean, Array, Object, Function, etc. After creating a variable, *
+ it is possible to change its assigned value and datatype.
+ * 1: In order to create a variable, we must first use the keyword var followed by *
+ a unique name(aka an id or alias) that will identify the variable.
+ * 2: A variable is capable of undergoing two phases: declaration and initialization *
+ (aka assignment).
+ * 3: Variables also have the capability to be reassigned to new, completely *
+ different values. 
+ * 4: There are three total keywords that can be used to declare variables: var, *
+ let, and const. Each keyword is different from the other, with its own set of *
+ features.  
+ * 5: After declaring a variable and executing your code, Javascript undergoes *
+  a process that pulls all of the declarations to the top of the code script. This *
+  process is called HOISTING. 
  */
+
+  // 1. Declaration //
+  /* We must use a keyword to first declare our variable, followed by a unique name *
+  that represents the intended value or purpose of the variable. If the variable is *
+  to be comprised of two words, it is a common practice to type out the variable name *
+  in camelCase. This is 
+   */
