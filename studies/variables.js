b55430bc12232fdf4e62a49a865343a71e3d5e27
Kevin Klein <kklein166@gmail.com>
2022-10-09 02:44:18 +0000

    studies - variables

--- studies/variables.js
+++ studies/variables.js
@@ -21,9 +21,47 @@
   process is called HOISTING. 
  */
 
-  // 1. Declaration //
-  /* We must use a keyword to first declare our variable, followed by a unique name *
-  that represents the intended value or purpose of the variable. If the variable is *
-  to be comprised of two words, it is a common practice to type out the variable name *
-  in camelCase. This is 
-   */
+// 1. Declaration //
+    /* We must use a keyword to first declare our variable, followed by a unique name *
+    that represents the intended value or purpose of the variable. If the variable is *
+    to be comprised of two words, it is a common practice to type out the variable name *
+    in camelCase. This is done by using lowercase for the first letter of the first word *
+    and uppercase for any word that follows, with no spaces in between. Here is an example: 
+    */
+    var myDog;
+
+    /* It is also important to note that there are some rules that apply when declaring a *
+    variable in javascript. 
+        /* The variable name must start with a letter, underscore, or dollar sign and cannot *
+        contain spaces. 
+        * We can use numbers after the first letter, such as myDog1.
+        * Variables are case-sensitive, meaning myDog and MyDog are two completely different *
+        variables.
+    In the example below, we use the keyword var to declare the variable. In this declaration *
+    phase, the variable firstName is considered undefined because it has not yet been *
+    initialized to any value. 
+    */
+    var firstName;
+    console.log(firstName); // prints => undefined
+
+// 2. Initialization/Assignment //
+    /* For the second phase of using a variable, we initialize the variable by assigning it *
+    to a specific value using the assignment operator (=).
+    */
+    firstName = "Kevin";
+    console.log(firstName); // prints => kevin
+    /* Declaration and initialization can also be done together within the same line of code:
+    */
+    var firstName = "Kevin";
+    console.log(firstName); // prints => Kevin
+
+// 3. Re-assignment //
+    /* A variable's value can be changed or updated later in our code. 
+     */
+    firstName = "Clem";
+
+// 4. Keywords //
+    /* Variables are not limited to being declared with the var keyword only. There are in *
+    fact a total of 3 keywords that we can use to declare a variable: var, let, const. Each *
+    of these keywords is different from the other in the way that they behave. 
+     */
