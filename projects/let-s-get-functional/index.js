767af1d6f2a8cfeec71c4ec1f6f857dda2ce13a7
Kevin Klein <kklein166@gmail.com>
2022-10-23 21:52:14 +0000

    adding pseudocode to let's get functional

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -114,32 +114,32 @@ var friendFirstLetterCount = function(array, customer, letter) { //create a func
 };
 
 var friendsCount = function(array, name) {
-    return _.reduce(array, function(result, obj, i, array){
-        for(let i = 0; i < obj.friends.length; i++) {
-            if(name === obj.friends[i].name) {
-                result.push(obj.name);
+    return _.reduce(array, function(result, obj, i, array){ // return the value of invoking _.reduce with the inputs array, a callback function with the inputs result, obj, i, and array, and a seed of an array literal
+        for(let i = 0; i < obj.friends.length; i++) { //iterate through the property friends array in current obj element
+            if(name === obj.friends[i].name) { // determine if input name is strictly equal to the name of the current iteration of property friends array in current obj element
+                result.push(obj.name); // push the value of the object name property into result
             }
         }
-        return result;
+        return result; //return result
     }, [])
 }
 
 
 var topThreeTags = function(array){
-    let map = {}; //create variable map 
-    _.each(array, function(obj){
-      _.each(obj.tags, function(tag){
-        map[tag] = (map[tag] || 0) + 1;
+    let map = {}; //create variable map and assign it to an object literal
+    _.each(array, function(obj){ //invoke _.each with the inputs array and a callback function with the input obj
+      _.each(obj.tags, function(tag){ //invoke _.each with the inputs obj,tags and a callback function with the input tag
+        map[tag] = (map[tag] || 0) + 1; // create a key tag in map object and assign it to the value of the key tag in map OR 0 plus 1
       })
     });
-    let topArray = _.map(Object.keys(map), obj => [obj, map[obj]]).sort((a, b) => a[1] - b[1]);
-    let output = topArray.slice(-3);
-    for(let i = 0; i < output.length; i++) {
-      output[i].pop();
+    let topArray = _.map(Object.keys(map), obj => [obj, map[obj]]).sort((a, b) => a[1] - b[1]); //create a variable topArray and assign it the value of invoking  _.map with the inputs of Object.keys(map) and a callback function that takes in the input obj. The callback function sorts the keys in obj in order
+    let output = topArray.slice(-3); // create variable output and assign it to the value of topArray sliced
+    for(let i = 0; i < output.length; i++) { //iterate through output
+      output[i].pop(); // pop off the current iteration of output
     }
-    let newArray = [];
-    newArray = newArray.concat(output[0], output[1], output[2]);
-    return newArray;
+    let newArray = []; //create variable newArray
+    newArray = newArray.concat(output[0], output[1], output[2]); //assign newArray the value of concating the first three items of output
+    return newArray; //return newArray
   };
 
 //     return function pluck(array, property) {
@@ -155,23 +155,23 @@ var topThreeTags = function(array){
 
 
 var genderCount = function(array){
-    return _.reduce(array, function(object, obj, i, array){
-        let females = _.filter(array, function(customer, index, array){
-            return customer.gender === 'female';
+    return _.reduce(array, function(object, obj, i, array){ //return the value of invoking reduce with the input array and a callback function with the inputs object, obj, i, and array, and a seed of an object literal
+        let females = _.filter(array, function(customer, index, array){ //create variable females and assign it to the value of invoking _.filter with the inputs array and a callback function with the inputs customer, index, and array
+            return customer.gender === 'female'; //return a filtered array, females, if customer property gender is strictly equal to 'female'
         });
-        object.female = females.length;
+        object.female = females.length; //create a female key in the result object and assign it to the length of females
 
-        let males = _.filter(array, function(customer, index, array){
-            return customer.gender === 'male';
+        let males = _.filter(array, function(customer, index, array){ //create variable males and assign it to the value of invoking _.filter with the inputs array and a callback function with the inputs customer, index, and array
+            return customer.gender === 'male'; //return a filtered array, males, if customer property gender is strictly equal to 'male'
         });
-        object.male = males.length; 
+        object.male = males.length; // create a male key in the result object and assign it to the length of males
 
-        let nonBinary = _.filter(array, function(customer, index, array){
-            return customer.gender === 'non-binary';
+        let nonBinary = _.filter(array, function(customer, index, array){ //create variable nonBinary and assign it to the value of invoking _.filter with the inputs array and a callback function with the inputs customer, index, and array
+            return customer.gender === 'non-binary'; //return a filtered array, nonBinary, if customer property gender is strictly equal to 'non-binary'
         });
-        object['non-binary'] = nonBinary.length;
+        object['non-binary'] = nonBinary.length; //create a non-binary key in the result object and assign it to the length of nonBinary
 
-        return object;
+        return object; //return object
     }, {});
 
 }
