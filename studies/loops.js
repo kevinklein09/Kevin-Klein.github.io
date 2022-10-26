1b77df832b5b77e045a900384ce033196708c1d4
Kevin Klein <kklein166@gmail.com>
2022-10-26 23:00:44 +0000

    studies - loops

--- studies/loops.js
+++ studies/loops.js
@@ -1,15 +1,11 @@
-/**
- * LOOPS
+/** LOOPS:
  * 
- * It is sometimes beneficial to repeat code in Javascript. Loops are 
+ * 0: It is sometimes beneficial to repeat code in Javascript. Loops are 
  * a fast and easy way to achieve this, as they allow us to run a block of code 
- * multiple times. 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ * multiple times. We will go over three different loops:
+ * 1: For Loops
+ * 2. For...in Loops
+ * 3: While Loops 
  */
+
+// 1. For Loops
