2d95814e02eb0a8233cea2a1213e363b5f941b67
Kevin Klein <kklein166@gmail.com>
2022-10-29 22:54:45 +0000

    studies - string-manipulation

--- studies/string-manipulation.js
+++ studies/string-manipulation.js
@@ -8,7 +8,7 @@
  */
 
 // 1. String Operators
-    /** Simply put, a string operator is an operator that operates on a string. Javascript supports
+    /* Simply put, a string operator is an operator that operates on a string. Javascript supports
      * two operators that can modify a string, which allow us to easily join one string to another:
      */
         /** CONCATENATE OPERATOR
@@ -30,3 +30,31 @@
          let str3 = 'Kevin';
          str3 += ' Klein';
          console.log(str3); // prints => 'Kevin Klein'
+
+// 2. String Methods
+    /* Javascript supports various methods to manipulate the value of a string. These methods have a variety
+     * of uses and each changes the string differently. Here are some of the most common methods used for 
+     * string manipulation:
+     */
+
+        /** CONCAT()
+         * Similar to the concat operator, the concat() method is used to join more than one string together and
+         * return a new string without changing the original string. Here's an example:
+         */
+
+        let str4 = 'Hello';
+        let str5 = "World!";
+        let str6 = str4.concat(' ', str5); // we must add a space in between str4 and str5
+        console.log(str6); // prints => 'Hello World!'
+        console.log(str4); // prints => 'Hello'
+
+        /** INDEXOF()
+         * The indexOf() method returns the position(index) of the first occurence of a value in a  string. If no 
+         * match is found, -1 is returned. The indexOf() method is case sensitive. Here's an example:
+         */
+
+        let str7 = "Hello Kevin, welcome to Javscript!";
+        let result = str7.indexOf('welcome');
+        console.log(result); // prints => 13
+        let result2 = str7.indexOf('kevin');
+        consolelog(result2); // prints => -1
